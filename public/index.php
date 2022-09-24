a8e089c280c3346c2b25aa715557599c069c54bd
Шкребень Евгений <evg.shkr@gmail.com>
2022-09-24 13:08:19 +0300

    lesson 5 finished

--- public/index.php
+++ public/index.php
@@ -7,8 +7,6 @@ if (PHP_MAJOR_VERSION < 8) {
 require_once dirname(__DIR__).'/config/init.php';
 
 new \wfm\App();
-echo \wfm\App::$app->getProperty('site_name');
-/*
-\wfm\App::$app->setProperty('test', 'TEST');
-var_dump(\wfm\App::$app->getProperties());
-*/
+
+
+throw new Exception('Ошибкус');
